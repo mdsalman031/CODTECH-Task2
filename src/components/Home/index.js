c94e8378514e47f86c19703857169aa998fdcb39
031mdsalman <031mdsalman@gmail.com>
2024-11-15 19:41:32 +0530

    Initial commit

---
+++ src/components/Home/index.js
@@ -0,0 +1,88 @@
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import LogoTitle from '../../assets/images/logo-s.png'
+import Logo from './Logo'
+import './index.scss'
+
+const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+
+  const nameArray = ['a', 'l', 'm', 'a', 'n']
+  const jobArray = [
+    'C',
+    'o',
+    'm',
+    'p',
+    'u',
+    't',
+    'e',
+    'r',
+    ' ',
+    'S',
+    'c',
+    'i',
+    'e',
+    'n',
+    'c',
+    'e',
+    ' ',
+    'S',
+    't',
+    'u',
+    'd',
+    'e',
+    'n',
+    't',
+    '.',
+  ]
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  return (
+    <>
+      <div className="container home-page">
+        <div className="text-zone">
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i,</span>
+            <br />
+            <span className={`${letterClass} _13`}>I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+            <img
+              src={LogoTitle}
+              alt="JavaScript Developer Name, Web Developer Name"
+            />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={nameArray}
+              idx={15}
+            />
+            <br />
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={jobArray}
+              idx={20}
+            />
+          </h1>
+          <h2>Web Developer / Proficient in React js, Node js and MongoDB</h2>
+          <Link to="/contact" className="flat-button">
+            CONTACT ME
+          </Link>
+        </div>
+        <Logo />
+      </div>
+
+      <Loader type="pacman" />
+    </>
+  )
+}
+
+export default Home
